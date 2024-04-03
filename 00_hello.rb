eb44d43da625893f9e874eeaa2a0eeb58708b3d4
SabrinaAEH <sabrina.aeh@gmail.com>
2024-04-03 13:26:15 +0200

    modif fichier 00_hello.rb

--- 00_hello.rb
+++ 00_hello.rb
@@ -3,3 +3,12 @@ def say_hello
 end
 
 puts say_hello
+
+def say_hello_name
+    puts "What's tour name?"
+    print ">"
+    first_name = gets.chomp
+    puts "Bonjour, #{first_name}"
+end
+
+puts say_hello_name
